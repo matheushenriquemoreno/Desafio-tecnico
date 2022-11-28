45d2687710b792c3890ea2c8a14a2e0d01e6b2c8
Matheus Henrique <69221000+matheushenriquemoreno@users.noreply.github.com>
2022-11-27 23:00:59 -0300

    Adicionando segundo projeto inicio

---
+++ Projeto2-pagina-estatica/PaginasEstaticas/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PaginasEstaticas.Models;
+
+namespace PaginasEstaticas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
